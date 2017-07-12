66e34333f3af3a410e7784f2e961792533af2caf
Angel Martinez-Perez <gelowin33@yahoo.es>
2017-07-13 00:08:04 +0200

    modificacion de casa

--- tercero.R
+++ tercero.R
@@ -7,3 +7,19 @@ eval `keychain --eval --agents ssh id_rsa`
 
 no funciona del todo bien, hay que ponerlo alguna ve
 
+ssh-keygen -t rsa -b 4096 -C "gelowin33@yahoo.es"
+cd .ssh/
+ll
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+cat id_rsa.pub 
+cat /etc/gitconfig
+cd ..
+ll
+mkdir git
+cd git
+ll
+git config --global user.name "Angel Martinez-Perez"
+git config --global user.email gelowin33@yahoo.es
+git config --global core.editor emacs
+exit
