75daa73ba5b36f8dcc955082703409ca4a404a8a
Angel Martinez-Perez <gelowin33@yahoo.es>
2017-07-13 12:32:09 +0200

    key-persistencey help

---
+++ keychain.R
@@ -0,0 +1,20 @@
+## eval "$(ssh-agent -s)"
+## ssh-add ~/.ssh/id_rsa
+## 
+## 
+## bajarse el keychain y poner esta linea en el .bashrc
+## ssh-agent persistency
+## eval `keychain --eval --agents ssh id_rsa`
+## 
+## git config --global user.name "Angel Martinez-Perez"
+## git config --global user.email gelowin33@yahoo.es
+## git config --global core.editor emacs
+## git config --list
+## 
+## Host github.com
+## 	HostName github.com
+## 	User git
+## 	IdentityFile ~/.ssh/id_rsa
+## 
+## chmod 600 ~/.ssh/config
+
