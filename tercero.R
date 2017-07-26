12eb32b4972c54fdc2382ca59ed189fc87edc1f9
Angel Martinez-Perez <gelowin33@yahoo.es>
2017-07-26 12:39:50 +0200

    t-sne

--- tercero.R
+++ tercero.R
@@ -4,52 +4,41 @@ library(plyr)
 
 load('/home/amartinezp/Documents/bestWorst/data/dat.kk.imp.nor.RData')
 
-## After imputation, I'm going to calculate the residuals, of the traits for all covariates that interact with each trait.
+row.names(dat) <- dat$Row.names
+dat <- dat[ , -c(which(colnames(dat)=='Row.names'))]
 
 
-## data preparation
-traits <- names(kk.imp.norm)
-dat <- dat[,-c(which(names(dat) %in% traits))]
-kk.imp.norm$id <- row.names(kk.imp.norm)
-
-dat <- join(dat,kk.imp.norm,by='id')
-row.names(dat) <- dat$id
-
-## data residualization
-library(solarius)
-## cargo datos
-source('/home/amartinezp/lib/load.fenoGAIT2.R')
-load.fenoGAIT2()
-
-traits <- setdiff(traits,c('CVI','VT','id'))
 
 
-i <- 1
+
+##############################################
+#######         t-SNE                 ########
+##############################################
+library(Rtsne)
+library(plyr)
 
 
-for(i in 2:length(traits)){
-    covs <- f1$infoTrait(traits[i])$covariates
-    covs <- intersect(covs,names(dat))
-    if(is.na(covs[1]))
-        next
-    
-    
-    formula <- as.formula(paste(traits[i],'~',paste(covs,collapse='+')))
-    
-    
-    mod.k <- solarPolygenic(formula=formula,data=dat,covtest=TRUE,screen = TRUE)
-    mod.k.resi <- mod.k$resf[,-2]
-    colnames(mod.k.resi)[2] <- traits[i]
-    dat <- dat[,-which(colnames(dat) == traits[i])]
-    dat <- join(dat,mod.k.resi,by='id')
+
+
+rtsneDims <- function(X){
+    a <- Rtsne(as.matrix(dat[dat$punctured==1,iruniverso.fen]),dims = X,pca=FALSE,pca_center=FALSE, theta=0.0)
+    colnames(a$Y) <- paste0('tsne.',X,'.',1:X)
+    row.names(a$Y) <- dat[dat$punctured==1,'id']
+    return(a)
 }
 
-
-save(dat,traits,universo.dis,universo.extr,universo.fen,universo,chungos,chungosMenos,out.plot,out.dat,file=file.path(out.dat,'dat.imp.nor.res.RData'))
-
+bunchRtsne <- llply(.fun = rtsneDims,2:4)
 
 
-
+a.li <-cbind(as.data.frame(bunchRtsne[[1]]$Y),
+            as.data.frame(bunchRtsne[[2]]$Y),
+            as.data.frame(bunchRtsne[[3]]$Y))
+rm(bunchRtsne)
+a.li$id <- row.names(a.li)
+dat <- join(dat,a.li,by='id')
+rm(a.li)
+row.names(dat) <- dat$id
+save(list=ls(),file=file.path(out.dat,'dat.imp.nor.rtsne.RData'))
 
 
 
