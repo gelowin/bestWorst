a13efa7fd37e9726eb42f936150a29e8f7de2998
Angel Martinez-Perez <gelowin33@yahoo.es>
2017-08-08 12:23:57 +0200

    file new autoencoder

---
+++ autoencoder.R
@@ -0,0 +1,8 @@
+## el autoencoder hace que en vez de tener grupos de individuos que desarrollan una enfermedad
+## lo que tendremos son características de estos individuos que nos hacen desarrollar la enfermedad.
+## por ejemplo nos extraerán características como, si hace ejercicio, si tiene componentes genéticas de riesgo, si ha tenido inmobilización.
+
+## se diferencia de un PCA en que en el PCA las características son siempre globales, en el autoencoder, la mayoría son locales.
+## se diferencia de un clustering en que el clustering hace grupos de individuos, en el autoencoder, hace grupos de características.
+
+## el PCA es un caso particular del autoencoder.
